f22e172d349edf9d94b310f844255ed9335a39ca
FMA07 <fe.mayoral@duocuc.cl>
2024-11-29 23:56:07 -0300

    Copiadas paginas base.html en appClientes y appVendedor. Cambiado reserva.html a appCliente

--- MovieNow/urls.py
+++ MovieNow/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include ('pagWeb.urls'))
+    path('', include ('pagWeb.urls')),
+    path('vendedor/', include ('appVendedor.urls')),
+    path('cliente/', include ('appCliente.urls')),
 ]
